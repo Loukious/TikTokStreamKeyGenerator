393aa4ec2c229287d2dd836c5e038f05853ad1b8
Loukious <Loukious@users.noreply.github.com>
2024-01-19 18:29:36 +0100

    Adding exception handling

--- TiktokStreamKeyGenerator.py
+++ TiktokStreamKeyGenerator.py
@@ -5,11 +5,17 @@ import sys
 class Stream:
     def __init__(self, sid_guard_cookie, title, game_tag_id, gen_replay=False, close_room_when_close_stream=True):
         self.streamInfo = self.createStream(sid_guard_cookie, title, game_tag_id, gen_replay, close_room_when_close_stream)
-        self.streamUrl = self.streamInfo["data"]["stream_url"]["rtmp_push_url"]
-        split_index = self.streamUrl.rfind("/")
-        self.baseStreamUrl = self.streamUrl[:split_index]
-        self.streamKey = self.streamUrl[split_index + 1:]
-        self.streamShareUrl = self.streamInfo["data"]["share_url"]
+        self.created = False
+        try:
+            self.streamUrl = self.streamInfo["data"]["stream_url"]["rtmp_push_url"]
+            split_index = self.streamUrl.rfind("/")
+            self.baseStreamUrl = self.streamUrl[:split_index]
+            self.streamKey = self.streamUrl[split_index + 1:]
+            self.streamShareUrl = self.streamInfo["data"]["share_url"]
+            self.created = True
+        except:
+            print(self.streamInfo["data"]["prompts"])
+
 
     def createStream(self, sid_guard_cookie, title, game_tag_id, gen_replay=False, close_room_when_close_stream=True):
         url = "https://webcast16-normal-c-useast2a.tiktokv.com/webcast/room/create/"
@@ -45,6 +51,7 @@ close_room_when_close_stream = sys.argv[5].lower() == 'true' if len(sys.argv) > 
 
 s = Stream(sid_guard_cookie, title, game_tag_id, gen_replay, close_room_when_close_stream)
 
-print(s.baseStreamUrl)
-print(s.streamKey)
-print(s.streamShareUrl)
+if s.created:
+    print(s.baseStreamUrl)
+    print(s.streamKey)
+    print(s.streamShareUrl)
